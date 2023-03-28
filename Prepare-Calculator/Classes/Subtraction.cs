1cf87a497e185d3e038f35649deb27f487a2b4e9
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2023-03-29 01:43:05 +0200

    khabir create subtraction

---
+++ Prepare-Calculator/Classes/Subtraction.cs
@@ -0,0 +1,17 @@
+﻿using Prepare_Calculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prepare_Calculator.Classes
+{
+    public  class Subtraction :IEquation
+    {
+        public double Perform(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
